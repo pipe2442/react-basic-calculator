582cd689b978f1c81dc05786900d854b0a7b4058
pipe2442 <felipeangulo2442@gmail.com>
2021-08-05 19:00:07 -0500

    Add iteration for component elements

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,38 +1,32 @@
 import React from 'react';
 import Button from './Button';
 
-const ButtonPanel = () => (
-  <div>
-    <tr>
-      <Button buttonName="AC" />
-      <Button buttonName="+/-" />
-      <Button buttonName="%" />
-      <Button buttonName="÷" />
-    </tr>
-    <tr>
-      <Button buttonName="7" />
-      <Button buttonName="8" />
-      <Button buttonName="9" />
-      <Button buttonName="X" />
-    </tr>
-    <tr>
-      <Button buttonName="4" />
-      <Button buttonName="5" />
-      <Button buttonName="6" />
-      <Button buttonName="-" />
-    </tr>
-    <tr>
-      <Button buttonName="1" />
-      <Button buttonName="2" />
-      <Button buttonName="3" />
-      <Button buttonName="+" />
-    </tr>
-    <tr>
-      <Button buttonName="0" />
-      <Button buttonName="." />
-      <Button buttonName="=" />
-    </tr>
-  </div>
-);
+const ButtonPanel = () => {
+  const names = [['AC', '+/-', '%', '÷'], ['7', '8', '9', '10', 'X'], [4, 5, 6, '-'], ['1', '2', '3', '+'], ['0', '.', '=']];
+
+  const renderButtons = (names) => (
+    names.map((name) => <Button key={name.id} buttonName={name} />)
+  );
+
+  return (
+    <div>
+      <tr>
+        {renderButtons(names[0])}
+      </tr>
+      <tr>
+        {renderButtons(names[1])}
+      </tr>
+      <tr>
+        {renderButtons(names[2])}
+      </tr>
+      <tr>
+        {renderButtons(names[3])}
+      </tr>
+      <tr>
+        {renderButtons(names[4])}
+      </tr>
+    </div>
+  );
+};
 
 export default ButtonPanel;
